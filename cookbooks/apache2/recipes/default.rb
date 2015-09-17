8c69cc8e70d8c9bd6616a383ce8342f6d3a6deb1
Nick <nma@hedgeserv.com>
2015-09-17 19:18:33 -0400

    Update default.rb

--- cookbooks/apache2/recipes/default.rb
+++ cookbooks/apache2/recipes/default.rb
@@ -1,1 +1,19 @@
 
+package 'apache2' do
+        action :install
+end
+
+template "/etc/apache2/sites-available2/000-default.conf" do
+	source "000-default.conf.erb"
+	mode "0755"
+end
+
+template "/etc/apache2/sites-enabled2/000-default.conf" do
+	source "000-default.conf.erb"
+	mode "0755"
+end
+
+service 'apache2' do
+  supports :status => true
+  action [:enable, :start]
+end
